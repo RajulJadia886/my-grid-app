7285dc502bf0f04a43e687f2eb2bb41088e79885
RAJUL JADIA <Rajuljadia31@outlook.com>
2022-07-29 16:02:22 +0530

    basic grid implementation done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageSettingsModel } from '@syncfusion/ej2-angular-grids';
+import {data}  from '../data'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`<ejs-grid [dataSource]='data' [allowGrouping]="true" [allowFiltering]="true" [allowSorting]="true" [allowPaging]="true" [pageSettings]='pageSettings'>
+    <e-columns>
+      <e-column field="OrderID" headerText='Product ID' textAlign="Left" width='80'></e-column>
+      <e-column field="CustomerID" headerText="Customer ID" textAlign="left"></e-column>
+      <e-column field="EmployeeID" headerText="Customer ID" textAlign="left"></e-column>
+      <e-column field="OrderDate" headerText="Order Date" textAlign="left"></e-column>
+      <e-column field="ShipName" headerText="Ship Name" textAlign="left"></e-column>
+      <e-column field="ShipCity" headerText="Ship City" textAlign="left"></e-column>
+      <e-column field="ShipAddress" headerText="Ship Address" textAlign="left" width="150"></e-column>
+      <e-column field="ShipRegion" headerText="Ship Region" textAlign="left"></e-column>
+      <e-column field="ShipPostalCode" headerText="Ship Postal Code" textAlign="left" width="150"></e-column>
+      <e-column field="ShipCountry" headerText="Ship Country" textAlign="left"></e-column>
+      <e-column field="Freight" textAlign="left"></e-column>
+      <e-column field="Verified" textAlign="left"></e-column>
+    </e-columns>
+  </ejs-grid>`,
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-grid-app';
+export class AppComponent implements OnInit{
+  public data: Object[] = []
+  public pageSettings: PageSettingsModel = {pageSize:5}
+  ngOnInit(): void {
+    this.data = data;
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GridModule,PagerModule,PageService,SortService,FilterService,GroupService } from '@syncfusion/ej2-angular-grids';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GridModule,
+    PagerModule
   ],
-  providers: [],
+  providers: [PageService,SortService,FilterService,GroupService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
